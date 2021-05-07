c4061ee64bba9783f3a7bdb7f0bfd33174d2e44c
Mikael Mello <git@mikaelmello.com>
2021-05-06 22:34:52 -0300

    Add helper functions and welcome message

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,8 @@
 use crate::Terminal;
-use std::io::{self, stdout, Write};
-use termion::{event::Key, input::TermRead, raw::IntoRawMode};
+use std::io::stdout;
+use termion::{event::Key, raw::IntoRawMode};
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn die(e: std::io::Error) {
     print!("{}", termion::clear::All);
@@ -37,7 +39,7 @@ impl Editor {
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
             _ => (),
@@ -46,29 +48,43 @@ impl Editor {
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        print!("{}", termion::clear::All);
+        Terminal::cursor_hide();
+        Terminal::cursor_position(0, 0);
 
         if self.should_quit {
+            Terminal::clear_screen();
             println!("Good bye!");
         } else {
             self.draw_rows();
-            print!("{}", termion::cursor::Goto(1, 1));
+            Terminal::cursor_position(0, 0);
         }
 
-        io::stdout().flush()
+        Terminal::cursor_show();
+        Terminal::flush()
+    }
+
+    fn draw_welcome_message(&self) {
+        let welcome_message = format!("Hecto editor -- version {}\r", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+
+        let mut welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}", welcome_message);
     }
 
     fn draw_rows(&self) {
-        for _ in 0..self.terminal.size().height {
-            println!("~\r");
-        }
-    }
-}
+        let height = self.terminal.size().height;
+        for row in 0..height - 1 {
+            Terminal::clear_current_line();
 
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
+            if row == height / 3 {
+                self.draw_welcome_message();
+            } else {
+                println!("~\r");
+            }
         }
     }
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,3 +1,11 @@
+use std::io::{self, stdout, Write};
+
+use termion::{
+    event::Key,
+    input::TermRead,
+    raw::{IntoRawMode, RawTerminal},
+};
+
 pub struct Size {
     pub width: u16,
     pub height: u16,
@@ -5,9 +13,13 @@ pub struct Size {
 
 pub struct Terminal {
     size: Size,
+    _stdout: RawTerminal<std::io::Stdout>,
 }
 
 impl Terminal {
+    /// # Errors
+    ///
+    /// Will return `std::io::Error` if it fails to get terminal size
     pub fn new() -> Result<Self, std::io::Error> {
         let size = termion::terminal_size()?;
 
@@ -16,10 +28,52 @@ impl Terminal {
                 width: size.0,
                 height: size.1,
             },
+            _stdout: stdout().into_raw_mode()?,
         })
     }
 
+    #[must_use]
     pub fn size(&self) -> &Size {
         &self.size
+    }
+
+    pub fn clear_screen() {
+        print!("{}", termion::clear::All);
+    }
+
+    pub fn cursor_position(x: u16, y: u16) {
+        let x = x.saturating_add(1);
+        let y = y.saturating_add(1);
+        print!("{}", termion::cursor::Goto(x, y));
+    }
+
+    /// # Errors
+    ///
+    /// Will return error when call to stdout().flush() fails
+    pub fn flush() -> Result<(), std::io::Error> {
+        io::stdout().flush()
+    }
+
+    /// # Errors
+    ///
+    /// Will never return Error for now
+    pub fn read_key() -> Result<Key, std::io::Error> {
+        loop {
+            if let Some(key) = io::stdin().lock().keys().next() {
+                return key;
+            }
+        }
+    }
+
+    pub fn cursor_hide() {
+        print!("{}", termion::cursor::Hide);
+    }
+
+    pub fn cursor_show() {
+        print!("{}", termion::cursor::Show);
+    }
+
+    pub fn clear_current_line() {
+        print!("{}", termion::clear::CurrentLine);
     }
 }
