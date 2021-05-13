e370c091741e6c05f2fccf4cc33d3c7d70081782
Mikael Mello <git@mikaelmello.com>
2021-05-12 21:47:31 -0300

    Chapter 4 done, Rust is great

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,10 @@
-use crate::Terminal;
-use std::io::stdout;
+use crate::{Document, Row, Terminal};
+use std::{
+    env,
+    io::stdout,
+    time::{Duration, Instant},
+    usize,
+};
 use termion::{event::Key, raw::IntoRawMode};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -9,6 +14,7 @@ fn die(e: std::io::Error) {
     panic!(e);
 }
 
+#[derive(Default)]
 pub struct Position {
     pub x: usize,
     pub y: usize,
@@ -18,14 +24,51 @@ pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
     cursor_position: Position,
+    document: Document,
+    offset: Position,
+    status_message: StatusMessage,
+}
+
+struct StatusMessage {
+    text: String,
+    time: Instant,
+}
+
+impl StatusMessage {
+    fn from(message: String) -> Self {
+        Self {
+            time: Instant::now(),
+            text: message,
+        }
+    }
 }
 
 impl Editor {
-    pub fn new() -> Self {
+    pub fn default() -> Self {
+        let args: Vec<String> = env::args().collect();
+        let mut initial_status = String::from("HELP: Ctrl-Q = quit");
+
+        let document = if args.len() > 1 {
+            let file_name = &args[1];
+            let doc = Document::open(&file_name);
+
+            if doc.is_ok() {
+                doc.unwrap()
+            } else {
+                initial_status = format!("ERR: Could not open file: {}", file_name);
+                Document::default()
+            }
+        } else {
+            Document::default()
+        };
+
         Self {
+            document,
             should_quit: false,
-            cursor_position: Position { x: 0, y: 0 },
             terminal: Terminal::new().expect("Failed to initialize terminal"),
+            offset: Position::default(),
+            cursor_position: Position::default(),
+            status_message: StatusMessage::from(initial_status),
         }
     }
 
@@ -59,40 +102,91 @@ impl Editor {
             | Key::PageDown => self.move_cursor(pressed_key),
             _ => (),
         }
+
+        self.scroll();
         Ok(())
     }
 
     fn move_cursor(&mut self, key: Key) {
+        let terminal_height = self.terminal.size().height as usize;
         let Position { mut x, mut y } = self.cursor_position;
-        let size = self.terminal.size();
-        let height = size.height.saturating_sub(1) as usize;
-        let width = size.width.saturating_sub(1) as usize;
+
+        let height = self.document.len();
+        let mut width = if let Some(row) = self.document.row(y) {
+            row.len()
+        } else {
+            0
+        };
 
         match key {
             Key::Up => y = y.saturating_sub(1),
             Key::Down => y = std::cmp::min(y.saturating_add(1), height),
-            Key::Left => x = x.saturating_sub(1),
-            Key::Right => x = std::cmp::min(x.saturating_add(1), width),
+            Key::Left => {
+                if x > 0 {
+                    x -= 1;
+                } else if y > 0 {
+                    y -= 1;
+                    x = if let Some(row) = self.document.row(y) {
+                        row.len()
+                    } else {
+                        0
+                    }
+                }
+            }
+            Key::Right => {
+                if x < width {
+                    x += 1;
+                } else if y < height {
+                    y += 1;
+                    x = 0;
+                }
+            }
 
             Key::Home => x = 0,
             Key::End => x = width,
-            Key::PageUp => y = 0,
-            Key::PageDown => y = height,
+            Key::PageUp => {
+                y = if y > terminal_height {
+                    y - terminal_height
+                } else {
+                    0
+                }
+            }
+            Key::PageDown => {
+                y = if y.saturating_add(terminal_height) < height {
+                    y + terminal_height as usize
+                } else {
+                    height
+                }
+            }
             _ => (),
         }
+
+        width = if let Some(row) = self.document.row(y) {
+            row.len()
+        } else {
+            0
+        };
+
+        if x > width {
+            x = width;
+        }
+
         self.cursor_position = Position { x, y };
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
-        Terminal::cursor_position(&Position { x: 0, y: 0 });
+        Terminal::cursor_position(&Position::default());
 
         if self.should_quit {
             Terminal::clear_screen();
             println!("Good bye!");
         } else {
             self.draw_rows();
-            Terminal::cursor_position(&self.cursor_position);
+            Terminal::cursor_position(&Position {
+                x: self.cursor_position.x.saturating_sub(self.offset.x),
+                y: self.cursor_position.y.saturating_sub(self.offset.y),
+            });
         }
 
         Terminal::cursor_show();
@@ -113,14 +207,58 @@ impl Editor {
 
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
-        for row in 0..height - 1 {
+        for terminal_row in 0..height - 1 {
             Terminal::clear_current_line();
 
-            if row == height / 3 {
+            if let Some(row) = self.document.row(terminal_row as usize + self.offset.y) {
+                self.draw_row(row);
+            } else if self.document.is_empty() && terminal_row == height / 3 {
                 self.draw_welcome_message();
             } else {
                 println!("~\r");
             }
+        }
+        self.draw_message_bar();
+    }
+
+    fn draw_row(&self, row: &Row) {
+        let width = self.terminal.size().width as usize;
+        let start = self.offset.x;
+        let end = self.offset.x + width;
+
+        let row = row.render(start, end);
+        println!("{}\r", row);
+    }
+
+    fn draw_message_bar(&self) {
+        Terminal::clear_current_line();
+
+        let message = &self.status_message;
+        if Instant::now() - message.time < Duration::new(5, 0) {
+            let mut text = message.text.clone();
+            text.truncate(self.terminal.size().width as usize);
+            print!("{}", text);
+        }
+    }
+
+    fn scroll(&mut self) {
+        let Position { x, y } = self.cursor_position;
+
+        let width = self.terminal.size().width as usize;
+        let height = self.terminal.size().height as usize;
+
+        let mut offset = &mut self.offset;
+
+        if y < offset.y {
+            offset.y = y;
+        } else if y >= offset.y.saturating_add(height) {
+            offset.y = y.saturating_sub(height).saturating_add(1);
+        }
+
+        if x < offset.x {
+            offset.x = x;
+        } else if x >= offset.x.saturating_add(width) {
+            offset.x = x.saturating_sub(width).saturating_add(1);
         }
     }
 }
